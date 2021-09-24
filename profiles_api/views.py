93d791ae5c95881f6ed2ac7389ad349af547466b
AitorPo <aitorpoquetgin@gmail.com>
2021-09-24 10:17:41 +0200

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -117,3 +120,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     # We need to put a coma (',') at the end of a tuple or it won't be created
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    # We enable the search of items by their name or email field
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES    
